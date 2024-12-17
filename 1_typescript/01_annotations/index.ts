c941236f880589f7735b08fed926a10459f58135
mThanuj <mullagurithanuj0@gmail.com>
2024-12-17 18:59:14 +0530

    First

---
+++ 1_typescript/01_annotations/index.ts
@@ -0,0 +1,20 @@
+// Annotations are used to specify the datatype of a variable for type safety
+//
+
+// Strings
+let myName: string = "Thanuj";
+myName = "Mullaguri";
+// myName = 21 // error
+console.log(myName);
+
+// Numbers
+let age: number = 20;
+age = 21;
+// age = true // error
+console.log(age);
+
+// Boolean
+let isEligible: boolean = true;
+isEligible = false;
+// isEligible = 21 // error
+console.log(isEligible);
---
+++ 1_typescript/02_type_inference/index.ts
@@ -0,0 +1,4 @@
+// Compiler tries to automatically determine the type of the variable
+// if not explicitly specified
+
+let tech = "TypeScript"; // infers it to be a string
